0b231fd5c89192b9c851dff4df56f1eb8c872218
Sam Smith <sam@mintcanary.com>
2019-03-03 11:57:32 +0000

    active button

--- selectors.js
+++ selectors.js
@@ -1,7 +1,8 @@
 module.exports = {
 	customSelectors: {
 		':--button': '.button',
-		':--button-primary': '&[data-action="primary"]',
+		':--button-primary-modifier': '[data-action="primary"]',
+		':--button-active-modifier': '[aria-pressed="true"]',
 		':--button-group': '.button-group'
 	}
 };
