7e585c33757cf0a40b1cc2de4432d27b5221b6b1
Sam Smith <sam@mintcanary.com>
2019-04-01 15:23:45 +0100

    alerts

--- selectors.js
+++ selectors.js
@@ -2,7 +2,10 @@ module.exports = {
 	customSelectors: {
 		':--alert': '.alert',
 		':--alert-icon': '.alert_icon',
+		':--alert-heading': '.alert_heading',
 		':--alert-body': '.alert_body',
+		':--alert-body-section': '.alert_section',
+		':--alert-body-section-long-modifier': '[data-type="long"]',
 		':--button': '.button',
 		':--button-primary-modifier': '[data-action="primary"]',
 		':--button-active-modifier': '[aria-pressed="true"]',
